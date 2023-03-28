c377b4615553365879d2c741d6ba3068a7f09f6c
Olumide Bambe <loyalsfc@gmail.com>
2023-03-28 22:37:07 +0100

    Added price filtering

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import Head from 'next/head'
 import Header from '../../components/header'
 import Footer from '../../components/footer'
@@ -8,12 +8,25 @@ import Link from 'next/link'
 import Layout from '../../components/layout'
 import ItemsCollection from '../../components/itemsCollection'
 import Image from 'next/image'
+import { Slider } from '@mui/material'
 
 function Products({products, param}) {
-    const [sortParam, setSortParam] = useState('Popularity')
+    const [fetchedProducts, setFetchedProducts] = useState(products)
+    const [sortParam, setSortParam] = useState('default');
+    const [sortText, setSortText] = useState('Popularity');
+    const [showSort, setShowSort] = useState(false)
     const categoryName = products?.[0]?.category?.name; 
+    const [value, setValue] = useState([0, 1000])
 
-    const productsItem = useMemo(() => products.map(item => {
+    const productsItem = useMemo(() => fetchedProducts.sort((a, b) => {
+        if(sortParam === "ascending"){
+            return b.price - a.price
+        } else if(sortParam === "descending"){
+            return a.price - b.price
+        } else {
+            return a.id - b.id
+        }
+    }).map(item => {
         const randomPercentage = Math.floor(Math.random() * 50)
         return(
             <li className={`${style.mainItemsWrap} ${style.mainProducts}`} key={item?.id}>
@@ -28,7 +41,7 @@ function Products({products, param}) {
                     <p>${item?.price}</p>
                     {randomPercentage != 0 &&
                         <p>
-                            <pan className={style.slashedPrice}>${((item?.price * randomPercentage) / 100 + item?.price).toFixed(0)}</pan>
+                            <span className={style.slashedPrice}>${((item?.price * randomPercentage) / 100 + item?.price).toFixed(0)}</span>
                             <span className={styles.percentageSlash}>-{randomPercentage}%</span>
                         </p>
                     }
@@ -36,7 +49,31 @@ function Products({products, param}) {
                 </article>
             </li>
         )
+    }), [fetchedProducts, sortParam])
+
+    const limitedStock = useMemo(()=>
+        products.map((item, index) => {
+            if(index > 10 && index < 20){
+                const randomPercentage = Math.floor(Math.random() * 50)
+                return <ItemsCollection item={item} styles={styles} percentageSlash={randomPercentage} />
+            }
     }), [products])
+
+    const priceFiltering = async() => {
+        const res = await fetch(`https://api.escuelajs.co/api/v1/products/?categoryId=${param}&price_min=${value[0]}&price_max=${value[1]}`)
+        const result = await res.json()
+        setFetchedProducts(result)
+    }
+
+    const changeSortParam = (e, sortParam) => {
+        setSortParam(sortParam);
+        setShowSort(false)
+        setSortText(e.target.textContent)
+    }
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     return (
     <div>
@@ -53,26 +90,54 @@ function Products({products, param}) {
                     <h4>Limited Stocks Deals</h4>
                     <div>
                         <ul className={styles.limitedStockContainer}>
-                            {
-                                products.map((item, index) => {
-                                    if(index > 10 && index < 20){
-                                        const randomPercentage = Math.floor(Math.random() * 50)
-                                        return <ItemsCollection item={item} styles={styles} percentageSlash={randomPercentage} />
-                                    }
-                                })
-                            }
+                            {limitedStock}
                         </ul>
                     </div>
                 </section>
 
                 <section className={styles.mainProductSection}>
-                    <aside className={styles.aside}></aside>
+                    <aside className={styles.aside}>
+                        <div className={styles.priceFiltering}>
+                            <h4 className={styles.priceFilteringHeader}>
+                                <span>PRICE ($)</span>
+                                <button onClick={priceFiltering}>APPLY</button>
+                            </h4>
+                            <Slider
+                                getAriaLabel={() => 'Price Range'}
+                                value={value}
+                                onChange={handleChange}
+                                valueLabelDisplay="auto"
+                                className={styles.sliderColor}
+                                min={0}
+                                max={1000}
+                            />
+                            <div className={styles.priceFilterInputs}>
+                                <input
+                                    type='number'
+                                    value={value[0]}
+                                    onChange={(e)=>setValue([e.target.value, value[1]])}
+                                />
+                                <input
+                                    type='number'
+                                    value={value[1]}
+                                    onChange={(e)=>setValue([value[0], e.target.value])}
+                                />
+                            </div>
+                        </div>
+                    </aside>
                     <div className={styles.mainProducts}>
                         <h3>
                             <span>{categoryName}</span>
-                            <p>Sort By: {sortParam}</p>
+                            <div className={styles.sortingWrapper}>
+                                <p style={{backgroundColor: showSort ? '#EDEDED' : null}} onClick={()=>setShowSort(!showSort)}>Sort By: {sortText}</p>
+                                {showSort && <ul className={styles.sortContainer}>
+                                    <li onClick={(event)=>changeSortParam(event, 'default')}>Popularity</li>
+                                    <li onClick={(event)=>changeSortParam(event, 'descending')}>Price: Low to High</li>
+                                    <li onClick={(event)=>changeSortParam(event, 'ascending')}>Price: High to Low</li>
+                                </ul>}
+                            </div>
                         </h3>
-                        <p className={styles.totalProducts}>{products.length} products found</p>
+                        <p className={styles.totalProducts}>{fetchedProducts.length} products found</p>
                         <ul className={styles.mainPageItems}>
                             {productsItem}
                         </ul>
