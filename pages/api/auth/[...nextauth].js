915f5d70d58aeb4347e410b78378f4aff56d4604
Olumide Bambe <loyalsfc@gmail.com>
2023-08-04 15:28:51 +0100

    Fix redirection error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -14,6 +14,13 @@ export const authOptions = {
         })
     ],
     callbacks: {
+        async redirect({ url, baseUrl }) {
+          // Allows relative callback URLs
+          if (url.startsWith("/")) return `${baseUrl}${url}`
+          // Allows callback URLs on the same origin
+          else if (new URL(url).origin === baseUrl) return url
+          return baseUrl
+        },
         async jwt({ token, account, profile }) {
             // Persist the OAuth access_token and or the user id to the token right after signin
             if (account) {
